c5dc8a83db18ad1b33c43585867965ed5a20b295
i-Coder <prateek@DESKTOP-IVORE20.localdomain>
2021-03-16 22:11:54 +0530

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+// api id = http://www.omdbapi.com/?i=tt3896198&apikey=71521f23
+
+const express = require('express');
+const request = require('request')
+const app = express();
+app.set("view engine", 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('homepage')
+    // res.send("hi")
+})
+app.get('/result', (req, res) => {
+    console.log(req.query.MovieName)
+    // res.send(`searched for ${req.query.MovieName}`)
+    const url = `http://www.omdbapi.com/?apikey=71521f23&s=${req.query.MovieName}`
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            res.render("result",{movieData: data})
+
+        } else {
+            res.send("Error")
+        }
+    })
+})
+
+app.get('/result/:id', (req, res) => {
+    // res.send(`searched for ${req.query.MovieName}`)
+    const url = `http://www.omdbapi.com/?apikey=71521f23&i=${req.params.id}`
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            res.render("knowmore",{data : data})
+
+        } else {
+            res.send("Error")
+        }
+    })
+})
+app.get("*",(req,res)=>{
+    // res.send('Go Back! Illegal request')
+    res.render('Illegal')
+
+})
+app.listen('3000', () => {
+    console.log("server started");
+})
